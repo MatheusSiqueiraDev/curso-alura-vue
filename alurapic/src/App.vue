f2d6df576ab1dbaaa395b9bf46dbd425fb019037
Matheus Siqueira <siqueira4503@gmail.com>
2021-08-05 14:35:47 -0300

    Nova att

---
+++ alurapic/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!--<h1 v-text="titulo"></h1>-->
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img v-bind:src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'Alurapic',
+      fotos: [],
+    }
+  },
+  created() {
+    alert("Funcionou!");
+  }
+}
+</script>
+
+<style>
+
+</style>
