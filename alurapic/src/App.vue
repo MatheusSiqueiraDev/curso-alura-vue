8083534e63a65a2c329c7cf615c68e2c8de9410d
Matheus Siqueira <siqueira4503@gmail.com>
2021-08-12 16:55:08 -0300

    promise

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -19,7 +19,8 @@ export default {
     }
   },
   created() {
-    alert("Funcionou!");
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    promise.then(res => res.json()).then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
 </script>
