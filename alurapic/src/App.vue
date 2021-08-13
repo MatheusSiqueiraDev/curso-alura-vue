20be7937a11f0fbc6462b10e63e4cbbabc49e18f
Matheus Siqueira <siqueira4503@gmail.com>
2021-08-13 02:51:06 -0300

    Criando componente compartilhado

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <div>
+  <div class="corpo">
     <!--<h1 v-text="titulo"></h1>-->
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img v-bind:src="foto.url" :alt="foto.titulo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+         <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" v-bind:src="foto.url" :alt="foto.titulo">
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+  components: {
+    'meu-painel': Painel
+  },
   data() {
     return {
       titulo: 'Alurapic',
@@ -26,5 +33,26 @@ export default {
 </script>
 
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
 
 </style>
