3dcc1bf755e6ef727316914258d6861d335ff9ee
Matheus Siqueira <siqueira4503@gmail.com>
2021-08-27 09:19:56 -0300

    Modificando app vue

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -31,4 +31,12 @@ export default {
     width: 96%;
     margin: 0 auto;
   }
+
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0;
+  }
+
+  .pagina-enter-active, .pagina-leave-active{
+    transition: opacity .4s;
+  }
 </style>
