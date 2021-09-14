88becf0190f738bdb9b680d0e8404510d7cce73d
Matheus Siqueira <siqueira4503@gmail.com>
2021-09-13 23:12:44 -0300

    Diretivas

--- alurapic/src/directives/Tranform.js
+++ alurapic/src/directives/Tranform.js
@@ -4,8 +4,14 @@ Vue.directive('meu-transform', {
     bind(el, binding, vnode) {
         let current = 0;
         el.addEventListener('dblclick', function() {
-            current += 90;
+            let incremento = binding.value || 90;
+            if(binding.modifiers.reverse) {
+                current -= incremento;
+            } else {
+                current += incremento;
+            }
             el.style.transform = `rotate(${current}deg)`;
+            if(binding.modifiers.animate) el.style.transition = `transform 0.5s`;
         });
     }
 });
