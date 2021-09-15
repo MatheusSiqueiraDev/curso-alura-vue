b82e81b659f8a88d4e1c9eae46a1fcae6e3aa185
Matheus Siqueira <siqueira4503@gmail.com>
2021-09-15 13:08:06 -0300

    Pagina cadastro

--- alurapic/src/directives/Tranform.js
+++ alurapic/src/directives/Tranform.js
@@ -5,13 +5,54 @@ Vue.directive('meu-transform', {
         let current = 0;
         el.addEventListener('dblclick', function() {
             let incremento = binding.value || 90;
-            if(binding.modifiers.reverse) {
-                current -= incremento;
-            } else {
-                current += incremento;
+            let efeito;
+            if(!binding.arg || binding.arg == 'rotate') {
+                if(binding.modifiers.reverse) {
+                    current -= incremento;
+                } else {
+                    current += incremento;
+                }
+                efeito = `rotate(${current}deg)`;
+            } else if(binding.arg == 'scale') {
+                efeito = `scale(${incremento})`;
             }
-            el.style.transform = `rotate(${current}deg)`;
+            el.style.transform = efeito;
             if(binding.modifiers.animate) el.style.transition = `transform 0.5s`;
         });
     }
 });
+/* Outro modo de usar diretiva 
+export default {
+    bind(el, binding, vnode) {
+        let current = 0;
+        el.addEventListener('dblclick', function() {
+            let incremento = binding.value || 90;
+            let efeito;
+            if(!binding.arg || binding.arg == 'rotate') {
+                if(binding.modifiers.reverse) {
+                    current -= incremento;
+                } else {
+                    current += incremento;
+                }
+                efeito = `rotate(${current}deg)`;
+            } else if(binding.arg == 'scale') {
+                efeito = `scale(${incremento})`;
+            }
+            el.style.transform = efeito;
+            if(binding.modifiers.animate) el.style.transition = `transform 0.5s`;
+        });
+    }
+}
+
+** No componente **
+<script>
+import transform from '../../directives/Transform';
+
+export default {
+    directives: {
+        'meu-transform': transform
+    }
+}
+
+</script>
+*/
